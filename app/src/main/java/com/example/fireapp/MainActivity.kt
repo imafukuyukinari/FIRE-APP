28ec1dab652872c408c8d5f25dbceb5beac9eaac
今福幸成 <>
2023-09-16 15:25:12 +0900

    不要なインポート削除、コード整形

--- app/src/main/java/com/example/fireapp/MainActivity.kt
+++ app/src/main/java/com/example/fireapp/MainActivity.kt
@@ -3,40 +3,21 @@ package com.example.fireapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.fireapp.ui.theme.FIREAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -63,29 +44,29 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
-    @Composable
-    fun bottomNavigation() {
-        val items = listOf(
-            BottomNavigationItem.FullFire,
-            BottomNavigationItem.SideFire,
-            BottomNavigationItem.Simulation,
-        )
+@Composable
+fun bottomNavigation() {
+    val items = listOf(
+        BottomNavigationItem.FullFire,
+        BottomNavigationItem.SideFire,
+        BottomNavigationItem.Simulation,
+    )
 
-        BottomNavigation(
-            backgroundColor = White,
-        ) {
-            items.forEach { item ->
-                BottomNavigationItem(
-                    icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
-                    label = { Text(text = item.title) },
-                    selectedContentColor = White,
-                    selected = false,
-                    onClick = {
-                        //...
-                    }
-                )
-            }
+    BottomNavigation(
+        backgroundColor = White,
+    ) {
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
+                label = { Text(text = item.title) },
+                selectedContentColor = White,
+                selected = false,
+                onClick = {
+                    //...
+                }
+            )
         }
     }
 }
